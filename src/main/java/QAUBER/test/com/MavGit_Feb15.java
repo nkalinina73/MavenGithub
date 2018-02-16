cf3c5dd10a742d54943151525a818cf53c15c66a
“nkalinina73” <nkalinina73@gmail.com>
2018-02-16 10:23:49 -0800

    com2

--- src/main/java/QAUBER/test/com/MavGit_Feb15.java
+++ src/main/java/QAUBER/test/com/MavGit_Feb15.java
@@ -9,13 +9,13 @@ public class MavGit_Feb15 {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Natalia\\Documents\\selenium-java-3.8.1\\chromedriver_win32\\chromedriver.exe");
         ChromeDriver mg = new ChromeDriver();
         mg.get("https://www.google.com/");
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
         mg.findElement(By.xpath("//*[@id=\"lst-ib\"]")).sendKeys("ivanka trump");
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
         mg.findElement(By.xpath("//*[@id=\"lst-ib\"]")).submit();
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
         //mg.close();
     }
